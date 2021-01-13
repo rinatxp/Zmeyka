e568a93ee284cc16f1e4c3f54a48637d5d4556d6
rinatxp <58555309+rinatxp@users.noreply.github.com>
2021-01-13 16:25:22 +0500

    Добавьте файлы проекта.

---
+++ Zmeyka/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Zmeyka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(70, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(152, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(977, 496);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(977, 30);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(65, 24);
+            this.toolStripMenuItem1.Text = "Меню";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(196, 26);
+            this.toolStripMenuItem2.Text = "Новая игра";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.MenuNewGame);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.ShortcutKeys = System.Windows.Forms.Keys.F2;
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(196, 26);
+            this.toolStripMenuItem3.Text = "Пауза";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.MenuPause);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 528);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Змейка";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ObrabotkaKlavish);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+    }
+}
+
---
+++ Zmeyka/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Zmeyka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Яблок: " + "0";
+            label2.Text = "00:00:00";
+        }
+
+        static byte poleX = 30, poleY = 25; //Размеры поля
+        float prirost = 0.1F; //Коэффициент плавности движения
+        int rost = 10;
+        int every10coord;
+        int pixelX, pixelY; //Размеры "пикселя"
+        float headX, headY; //Координаты головы
+        byte moveWay; //Направление движения
+        byte appleNumber; //Кол-во яблок
+        byte isGamePlay = 0; //Идет ли игра
+        byte zaderzhkaKlavish = 0; //Создает задержку обработчика клавиш
+        byte[,] Maze = new byte[poleX, poleY]; //Поле
+
+        TimeSpan time;  //Значение времени
+        Random rand1 = new Random((int)DateTime.Now.Ticks);
+
+        List<Coords> snakeCoord = new List<Coords>(); //Координты змеи
+        List<Coords> snake10CoordList = new List<Coords>();
+        Coords appleCoord; //Координаты яблока
+
+        private void MenuNewGame(object sender, EventArgs e)
+        {
+            snakeCoord.Clear(); //Очищаем List змеи
+            snake10CoordList.Clear();
+            appleNumber = 0; //Обнуляем кол-во яблок
+            time = new TimeSpan(0, 0, 0); //Обнуляем время
+            label1.Text = "Яблок: " + appleNumber.ToString();
+            label2.Text = "00:00:00";
+            every10coord = 0;
+
+            Graphics G = pictureBox1.CreateGraphics();
+            pixelX = pictureBox1.Width / poleX; //Ширина пикселей
+            pixelY = pictureBox1.Height / poleY; //Высота пикселей
+
+            imageList1.ImageSize = new Size(pixelX, pixelY);  //Подключаем текстуры
+            imageList1.Images.Add(Image.FromFile("pictures\\area.jpg"));
+            imageList1.Images.Add(Image.FromFile("pictures\\snake.jpg"));
+            imageList1.Images.Add(Image.FromFile("pictures\\apple.jpg"));
+
+            for (int i = 0; i < poleX; i++)
+            {
+                for (int j = 0; j < poleY; j++)
+                    G.DrawImage(imageList1.Images[0], i * pixelX, j * pixelY);
+            } //Заполняем поле
+
+            for (float a = 10; a < 14; a += prirost)
+            {
+                if (a > 11.2)
+                    G.DrawImage(imageList1.Images[1], 10 * pixelX, a * pixelY);
+                snakeCoord.Add(new Coords(10 * pixelX, (poleY - a) * pixelY));
+            } //Создаем змею
+
+            snake10CoordList.Add(new Coords(10 * pixelX, (13) * pixelY));
+            snake10CoordList.Add(new Coords(10 * pixelX, (12) * pixelY));
+            snake10CoordList.Add(new Coords(10 * pixelX, (11) * pixelY));
+            snake10CoordList.Add(new Coords(10 * pixelX, (10) * pixelY));
+
+            appleCoord = CreateApple(); //Создаем яблоко
+            G.DrawImage(imageList1.Images[2], appleCoord.X * pixelX, appleCoord.Y * pixelY);
+
+            headX = 10.0F;
+            headY = 13.0F; //Начальные координаты головы
+            moveWay = 2; //Начальное направление
+            timer1.Interval = 12;
+            timer2.Interval = 1000;
+            timer1.Start(); // Запускаем таймер
+            timer2.Start();
+
+            isGamePlay = 1; //Игра запущена
+        }
+
+        private void MenuPause(object sender, EventArgs e)
+        {
+            int t = 0;
+            if (isGamePlay == 1) //Если игра была запущена
+                t = 1;
+            timer1.Stop(); timer2.Stop();
+            string msg = "Змейка v1.0\nАвтор: Хайретдинов Р.И.";
+            MessageBox.Show(msg, "О программе",  //Выводим сообщение
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (t == 1)
+            {
+                timer1.Start();
+                timer2.Start();
+            }
+        }
+
+        private void Timer1(object sender, EventArgs e)
+        {
+            Graphics G = pictureBox1.CreateGraphics();
+
+            switch (moveWay)
+            {
+                case 0: //Вверх
+                    headY = (float)(headY - prirost);
+                    if (headY < 0)
+                        headY = poleY;
+                    break;
+                case 1: //Вправо
+                    headX = (float)(headX + prirost);
+                    if (headX >= poleX)
+                        headX = 0;
+                    break;
+                case 2: //Вниз
+                    headY = (float)(headY + prirost);
+                    if (headY >= poleY)
+                        headY = 0;
+                    break;
+                case 3: //Влево
+                    headX = (float)(headX - prirost);
+                    if (headX < 0)
+                        headX = poleX;
+                    break;
+            } //Вычисляем следующую координату змеи
+
+            Coords c = new Coords(headX * pixelX, headY * pixelY); //Новые координаты
+            G.DrawImage(imageList1.Images[0], snakeCoord[snakeCoord.Count - 1].X, snakeCoord[snakeCoord.Count - 1].Y);
+            snakeCoord.Insert(0, c); // Увеличиваем змею на один сегмент  
+
+            Stolknovenie(); //Функция обработки столкновений
+
+            if (every10coord == 10)
+            {
+                snake10CoordList.Insert(0, c);
+                every10coord = 0;
+                if (rost == 0)
+                    snake10CoordList.RemoveAt(snake10CoordList.Count - 1);
+            } //Запоминаем каждую десятую координату змеи
+            every10coord++;
+
+            if (isGamePlay == 1) //Если игра запущена
+            {
+                G.DrawImage(imageList1.Images[1], snakeCoord[0].X, snakeCoord[0].Y); //Рисуем новый сегмент
+
+                if (rost > 0)
+                {
+                    G.DrawImage(imageList1.Images[0], snakeCoord[snakeCoord.Count - 1].X, snakeCoord[snakeCoord.Count - 1].Y);
+                    rost--;
+                }
+            }
+            zaderzhkaKlavish = 0; //Можно считать следующую нажатую клавишу
+        }
+
+        private void Timer2(object sender, EventArgs e)
+        {
+            Graphics G = pictureBox1.CreateGraphics();
+            time += new TimeSpan(0, 0, 1);  //Прибавляем секунду
+            label2.Text = time.ToString();  //Выводим
+        }
+
+        private void ObrabotkaKlavish(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)  //Нажатие стрелки
+            {
+                case Keys.Up:  //Вверх
+                    {
+                        if (moveWay != 2 && zaderzhkaKlavish == 0)
+                            moveWay = 0;
+                        zaderzhkaKlavish = 1; //Больше нельзя считывать нажатие до того как не пройдет тик таймера змеи
+                        break;
+                    }
+                case Keys.Right:  //Вправо
+                    {
+                        if (moveWay != 3 && zaderzhkaKlavish == 0)
+                            moveWay = 1;
+                        zaderzhkaKlavish = 1;
+                        break;
+                    }
+                case Keys.Down:  //Вниз
+                    {
+                        if (moveWay != 0 && zaderzhkaKlavish == 0)
+                            moveWay = 2;
+                        zaderzhkaKlavish = 1;
+                        break;
+                    }
+                case Keys.Left:  //Влево
+                    {
+                        if (moveWay != 1 && zaderzhkaKlavish == 0)
+                            moveWay = 3;
+                        zaderzhkaKlavish = 1;
+                        break;
+                    }
+            }
+        }
+
+        Coords CreateApple()
+        {
+        g:
+            int N1 = (rand1.Next(poleX - 4) + 3);
+            int M1 = (rand1.Next(poleY - 4) + 3);
+            Coords newAppleCoor = new Coords(N1, M1);
+            for (int b = 0; b < snake10CoordList.Count; b++)
+                if (Math.Abs(newAppleCoor.X * pixelX - snake10CoordList[b].X) <= 2 * pixelX && Math.Abs(newAppleCoor.Y * pixelY - snake10CoordList[b].Y) <= 2 * pixelY)
+                    goto g;
+            return newAppleCoor;
+        } //Создание координат яблок
+
+        void Stolknovenie()
+        {
+            int b = 0;
+            float c = 0;
+            float vis1 = 0;
+            float vis2 = 0;
+
+            switch (moveWay)
+            {
+                case 0: //Вверх
+                    c = snakeCoord[0].X;
+                    b = pixelX;
+                    vis1 = 0;
+                    vis2 = pixelY;
+                    break;
+                case 1: //Вправо
+                    b = pixelY;
+                    c = snakeCoord[0].Y;
+                    vis1 = pixelX;
+                    vis2 = 0;
+                    break;
+                case 2: //Вниз
+                    c = snakeCoord[0].X;
+                    b = pixelX;
+                    vis1 = pixelY;
+                    vis2 = 0;
+                    break;
+                case 3: //Влево
+                    b = pixelY;
+                    c = snakeCoord[0].Y;
+                    vis1 = 0;
+                    vis2 = pixelX;
+                    break;
+            }
+
+            for (float a = c; a <= c + b; a += b)
+            {
+                if (moveWay == 0 || moveWay == 2)
+                {
+                    if (Math.Round(snakeCoord[0].Y + vis1) == Math.Round(appleCoord.Y * pixelY + vis2))
+                        if (a >= appleCoord.X * pixelX && a <= appleCoord.X * pixelX + pixelX)
+                        {
+                            rost += 10 / (int)(prirost * 10);
+                            Yabl();
+                        }
+                } //Если вверх или вниз
+                else
+                {
+                    if (Math.Round(snakeCoord[0].X + vis1) == Math.Round(appleCoord.X * pixelX + vis2))
+                        if (a >= appleCoord.Y * pixelY && a <= appleCoord.Y * pixelY + pixelY)
+                        {
+                            rost += 10 / (int)(prirost * 10);
+                            Yabl();
+                        }
+                } //Если вправо или влево
+            } //Съедание яблока
+
+            for (int k = 0; k < snake10CoordList.Count - 1; k++)
+                for (float a = c; a <= c + b; a += b)
+                {
+                    if (moveWay == 0 || moveWay == 2)
+                    {
+                        if (Math.Round(snakeCoord[0].Y + vis1) == Math.Round(snake10CoordList[k].Y + vis2))
+                            if (a >= snake10CoordList[k].X && a <= snake10CoordList[k].X + pixelX)
+                                GameOver();
+                    } //Если вверх или вниз
+                    else
+                    {
+                        if (Math.Round(snakeCoord[0].X + vis1) == Math.Round(snake10CoordList[k].X + vis2))
+                            if (a >= snake10CoordList[k].Y && a <= snake10CoordList[k].Y + pixelY)
+                                GameOver();
+                    } //Если вправо или влево
+                } //Столкновение с телом
+
+            if (rost == 0)
+            {
+                snakeCoord.RemoveAt(snakeCoord.Count - 1);
+            } //Если яблоко не было съедено
+        } //Взаимодействие при столкновении с различными объектами
+
+        void GameOver()
+        {
+            timer1.Stop();  //Останавливаем игру
+            timer2.Stop();  //Останавливаем время
+            isGamePlay = 0;  //Игра остановлена
+            string msg = "Собрано яблок: ";
+            msg += appleNumber;
+            msg += "\nЗа время: ";
+            msg += label2.Text;
+            MessageBox.Show(msg, "Game over",  //Выводим сообщение (Game over)
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }  //Game over
+
+        void Yabl()
+        {
+            Graphics G = pictureBox1.CreateGraphics();
+            G.DrawImage(imageList1.Images[0], appleCoord.X * pixelX, appleCoord.Y * pixelY); //Рисуем
+            appleCoord = CreateApple(); //Создаем новое
+            G.DrawImage(imageList1.Images[2], appleCoord.X * pixelX, appleCoord.Y * pixelY); //Рисуем яблоко
+            appleNumber++;
+            label1.Text = "Яблок: " + appleNumber.ToString();
+        } //Рисуем яблоко
+
+        public class Coords
+        {
+            public float X;
+            public float Y;
+            public Coords(float x, float y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
+
